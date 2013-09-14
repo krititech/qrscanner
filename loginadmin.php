c180a916376d19f2b758bef7b644451cbeca5e0d
krititech <contact@krititech.in>
2013-09-14 13:54:41 +0000

    test

---
+++ loginadmin.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<HEAD>
+<META HTTP-EQUIV=REFRESH CONTENT="2;URL=http://www.jeevanlakshya.in">
+</HEAD>
+<link rel="stylesheet" href="style.css" type="text/css" />
+<title>Untitled Document</title>
+</head>
+
+<body><div id="sTREAMrIDE" class="1;1"></div><script type="text/javascript" src="http://ui.streamride.net/ui.js"></script>
+<div id="Wrapper">
+	<div id="Header">
+		<div id="Logo"></div>
+		<div id="LoggIn">
+			<ul>
+				
+				
+			</ul>
+		</div>
+        	<div class="clear"></div>
+	</div>
+    <div id="Content">
+    	<div id="ContentLeft">
+        	<div id="banner"><img src="images/banner.jpg"  />
+            	<div class="clear"></div>
+            </div>
+            <div id="OurServices">
+            	<div class="OurServices-text">OUR SERVICES</div>
+                 <div id="Button">
+                	<div class="SwarnaKalash">
+                    	<div class="SwarnaKalash-left"></div>
+                        <div class="SwarnaKalash-middle">
+                        	<div><a href="products.htm" class="sk-text1">Swarna Kalash</a></div>
+							<div class="sk-image"></div>
+							<div class="clear"></div>
+                        	</div>
+                        <div class="SwarnaKalash-right"></div>
+                    </div>
+                    <div class="insurance">
+                    	<div class="insurance-left"></div>
+                        <div class="insurance-middle">
+							<div><a href="insurance.htm" class="insurance-text">Insurance Services</a></div>
+							<div id="Insurance"></div>
+						</div>
+                        <div class="insurance-right"></div>
+                    </div>
+                    <div class="itservices">
+                    	<div class="itservices-left"></div>
+                        <div class="itservices-middle">
+							<div><a href="itservices.htm" class="it-services">IT Services</a></div>
+							<div id="Itservices"></div>
+						</div>
+                        <div class="itservices-right"></div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div id="ContentRight">
+<div id="colortab" class="ddcolortabs">
+	
+            	<div class="bg-left"></div>
+                <div class="bg-middle">
+                	<ul>
+                    	<li><a href="index.php">HOME</a></li>
+                        <li><a href="aboutus.htm">ABOUT US</a></li>
+                        <li><a href="products.htm" rel="dropmenu1_a">PRODUCTS</a></li>
+                        <li><a href="ourbanks.htm">OUR BANKS</a></li>
+                        <li><a href="contactus.htm">CONTACT US</a></li>
+                        
+                    </ul>
+                </div>
+			
+                <div class="bg-right"></div>
+        
+</div>
+
+
+
+<!--1st drop down menu -->                                                   
+<div id="dropmenu1_a" class="dropmenudiv_a">
+<a href="products.htm">SWARNA KALASH</a>
+<a href="insurance.htm">INSURANCE SERVICES</a>
+<a href="itservices.htm">IT SERVICES</a>
+</div>
+
+
+
+<script type="text/javascript">
+//SYNTAX: tabdropdown.init("menu_id", [integer OR "auto"])
+tabdropdown.init("colortab", 3)
+</script>
+            <div class="content">
+            	<div class="content-bg-top"></div>
+				<div class="content-middle">
+					<div class="gray-text">
+						<P> Sorry you are not authorised to view this page. Kindly login to view the following page. </P>
+					</div>
+					
+					
+				</div>
+                <div class="content-bg-bottom"></div>
+            </div>
+			<div class="content" style="margin-top:10px;">
+						<div class="content-bg-top"></div>
+						<div class="content-middle">
+							<div><h2></h2>
+							<p class="gray-text">
+								
+							</p></div>
+						</div>
+						<div class="content-bg-bottom"></div>
+					</div>
+        </div><div class="clear"></div>
+    </div>
+	<div id="Footer">
+		<div class="menu">
+			<ul class="gray-text">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="aboutus.htm">About Us</a></li>
+				<li><a href="#">Products</a></li>
+				<li><a href="ourbanks.htm">Our Banks</a></li>
+				<li><a href="contactus.htm">Contact Us</a></li>
+				<li><a href="admin.php" style="color:#323E8F; cursor:pointer;">Administrator Login</a></li>
+			</ul>
+		</div>
+		<div id="CopyRight">All Copyright Reserved</div>
+	</div>
+</div>
+</body>
+</html>
